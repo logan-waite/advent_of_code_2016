6ed0c2ee325efe483d5dea4a05ef3edda8dac782
Logan waite <lwaite2010@gmail.com>
2016-12-14 16:01:57 -0700

    Finished Day 7 part 1

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -1,41 +1,25 @@
 import re
-abba = re.compile(r"([a-z])([a-z])\2\1")
+abbaRe = re.compile(r"([a-z])([a-z])\2\1")
 badAbba = re.compile(r"([a-z])\1{3}")
 insideBrackets = re.compile(r"(?:\[[a-z]*)(([a-z])([a-z])\3\2)(?:[a-z]*\])")
 
-puzzleInput = open('puzzle.txt', 'r')
+r"\[[a-z]*(([a-z])([a-z])\3\2)[a-z]*\]"
+r"\[[a-z]*(([a-z])([a-z])\3\2)[a-z]*\]"
 
+r"([a-z])\1{3}"
+r"([a-z])\1{3}"
+puzzleInput = open('puzzle.txt', 'r')
 supported = 0
-notSupported = 0
-count = 0
-badABBA = 0
-inside = 0
-abbaMatch = 0
-good = 0
 
 for line in puzzleInput:
-    count += 1
 
     if badAbba.search(line):
-        badABBA += 1
-    if insideBrackets.search(line):
-        inside += 1
-    if abba.search(line):
-        abbaMatch += 1
-    if not badAbba.search(line) and not insideBrackets.search(line):
-        good += 1
-        print(line)
-        if abba.search(line):
-            supported += 1
-        else:
-            notSupported += 1
-    else:
-        notSupported += 1
+        matchString = badAbba.search(line)
+        line = badAbba.sub(",", line)
 
-print("badABBA %s" % badABBA)
-print("inside brackets %s" % inside)
-print("abbaMatch %s" % abbaMatch)
-print("good %s" % good)
+    if abbaRe.search(line):
+        abbaString = abbaRe.search(line)
+        if not insideBrackets.search(line):
+            supported += 1
+
 print("supported %s " % supported)
-print("not supported %s" % notSupported)
-print("count: %s" % count)
